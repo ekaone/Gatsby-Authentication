b669c3494fd0e822a4ea6b7613d84329e22939fe
Eka Prasetia <ekaone3033@gmail.com>
2020-06-08 11:55:44 +0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-cce5a8c0705f0d80f829.js"
+    "url": "webpack-runtime-0403f57a46b12f4509ba.js"
   },
   {
     "url": "framework-e84382e1b96b75949e61.js"
@@ -39,7 +39,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "dc0eb57c162381420c47f84dcd00449c"
+    "revision": "3ea4fc715fb51717d59f9547a4a98a5e"
   },
   {
     "url": "page-data/offline-plugin-app-shell-fallback/page-data.json",
@@ -47,7 +47,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "1ad9b67926fb7e9b97ab7bb6c0248570"
+    "revision": "4f68dd223b11cce98ded64894291ed79"
   },
   {
     "url": "manifest.json",
