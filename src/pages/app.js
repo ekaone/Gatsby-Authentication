18b702a77af38959f62932f017a9c85fb3b035d5
Eka Prasetia <ekaone3033@gmail.com>
2020-06-08 11:27:44 +0700

    add route basename

--- src/pages/app.js
+++ src/pages/app.js
@@ -7,7 +7,7 @@ import Login from "../components/login";
 
 const App = () => (
   <Layout>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <PrivateRoute path="/app/profile" component={Profile} />
       <Login path="/app/login" />
     </Router>
